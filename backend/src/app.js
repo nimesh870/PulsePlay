c0e353df064eef8167565d933a3def695ad9fe74
Nimesh Dulal <188695939+nimesh870@users.noreply.github.com>
2026-08-04 19:49:31 +0545

    adding cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // CORS
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://pulse-play-nimesh-dulals-projects.vercel.app",
     credentials: true
 }));
 
